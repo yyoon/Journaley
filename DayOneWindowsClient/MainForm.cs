3b3e2d8956e6a1be63beccf431bfe6c1feb28edc
YoungSeok Yoon <nullstein@gmail.com>
2012-01-17 11:04:10 -0500

    Initial Commit - Base Windows Forms Application along with the icons

---
+++ DayOneWindowsClient/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DayOneWindowsClient.Properties;
+
+namespace DayOneWindowsClient
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Icon = Resources.MainIcon;
+        }
+    }
+}
