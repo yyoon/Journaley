0004cb995e588c70afabff74145b25e8ddb19eaf
YoungSeok Yoon <nullstein@gmail.com>
2013-08-15 12:38:45 -0400

    Renamed files / directories / namespaces, etc.

---
+++ Journaley/Forms/PasswordInputForm.cs
@@ -0,0 +1,85 @@
+﻿namespace Journaley.Forms
+{
+    using System.Windows.Forms;
+    using Journaley.Models;
+
+    /// <summary>
+    /// The password input form shown when the user returns from other applications.
+    /// </summary>
+    public partial class PasswordInputForm : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PasswordInputForm"/> class.
+        /// </summary>
+        /// <param name="passwordVerifier">The password verifier.</param>
+        public PasswordInputForm(IPasswordVerifier passwordVerifier)
+        {
+            this.InitializeComponent();
+
+            this.Icon = Properties.Resources.MainIcon;
+            this.PasswordVerifier = passwordVerifier;
+        }
+
+        /// <summary>
+        /// Gets or sets the password verifier.
+        /// </summary>
+        /// <value>
+        /// The password verifier.
+        /// </value>
+        private IPasswordVerifier PasswordVerifier { get; set; }
+
+        /// <summary>
+        /// Handles the KeyDown event of the textBoxPassword control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyEventArgs"/> instance containing the event data.</param>
+        private void TextBoxPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Enter:
+                    {
+                        if (this.PasswordVerifier.VerifyPassword(this.textBoxPassword.Text))
+                        {
+                            this.DialogResult = System.Windows.Forms.DialogResult.OK;
+                            this.Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show(this.ParentForm, "Wrong Password!", "Day One Windows Client", MessageBoxButtons.OK);
+                            this.textBoxPassword.SelectAll();
+                            this.textBoxPassword.Focus();
+                        }
+
+                        e.Handled = true;
+                        break;
+                    }
+
+                case Keys.Escape:
+                    {
+                        this.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+                        this.Close();
+
+                        e.Handled = true;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Handles the KeyPress event of the textBoxPassword control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyPressEventArgs"/> instance containing the event data.</param>
+        private void TextBoxPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case (char)Keys.Enter:
+                case (char)Keys.Escape:
+                    e.Handled = true;
+                    break;
+            }
+        }
+    }
+}
---
+++ Journaley/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Journaley
+{
+    using System;
+    using System.Windows.Forms;
+    using Journaley.Forms;
+
+    /// <summary>
+    /// Main entry point
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
