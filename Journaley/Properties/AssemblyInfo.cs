aac6e0031a137d1073c642bc14c52d5b1ee98f26
YoungSeok Yoon <youngseok@cs.cmu.edu>
2014-02-04 20:36:09 -0500

    Bump up the version number to v1.1

--- Journaley/Properties/AssemblyInfo.cs
+++ Journaley/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("nullstein")]
 [assembly: AssemblyProduct("Journaley")]
-[assembly: AssemblyCopyright("Copyright ©  2012-2013")]
+[assembly: AssemblyCopyright("Copyright ©  2012-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Journaley.Test")]
