f97b6a57cd0627f36311de6c5d01ec32f25c2644
YoungSeok Yoon <nullstein@gmail.com>
2014-06-11 00:40:52 -0400

    Transparent table layout added

---
+++ Journaley/Controls/TransparentTableLayoutPanel.cs
@@ -0,0 +1,31 @@
+﻿namespace Journaley.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// TableLayoutPanel with a transparent background.
+    /// </summary>
+    public class TransparentTableLayoutPanel : TableLayoutPanel
+    {
+        /// <summary>
+        /// Gets the create parameters object.
+        /// </summary>
+        /// <value>
+        /// The create parameters object with the transparent flag added.
+        /// </value>
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00000020;
+
+                return cp;
+            }
+        }
+    }
+}
