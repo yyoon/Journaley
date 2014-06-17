31fda84a8a71b38aa756a874882c6e3fa50cc1bc
YoungSeok Yoon <nullstein@gmail.com>
2014-06-17 10:55:43 -0400

    HitTestValues added for manually sending the WM_NCLBUTTONDOWN event.

--- Journaley/Utilities/PInvoke.cs
+++ Journaley/Utilities/PInvoke.cs
@@ -27,6 +27,9 @@
         public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
         public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("user32.dll")]
@@ -74,6 +77,36 @@
             /// such as WPF windows with AllowsTransparency="true"
             /// </summary>
             CAPTUREBLT = 0x40000000
+        }
+
+        public static readonly int WM_NCLBUTTONDOWN = 0x00A1;
+
+        public enum HitTestValues : int
+        {
+            HTERROR = -2,
+            HTTRANSPARENT = -1,
+            HTNOWHERE = 0,
+            HTCLIENT = 1,
+            HTCAPTION = 2,
+            HTSYSMENU = 3,
+            HTGROWBOX = 4,
+            HTMENU = 5,
+            HTHSCROLL = 6,
+            HTVSCROLL = 7,
+            HTMINBUTTON = 8,
+            HTMAXBUTTON = 9,
+            HTLEFT = 10,
+            HTRIGHT = 11,
+            HTTOP = 12,
+            HTTOPLEFT = 13,
+            HTTOPRIGHT = 14,
+            HTBOTTOM = 15,
+            HTBOTTOMLEFT = 16,
+            HTBOTTOMRIGHT = 17,
+            HTBORDER = 18,
+            HTOBJECT = 19,
+            HTCLOSE = 20,
+            HTHELP = 21
         }
     }
 }
