aaae75f897f632ae6a945e7fd9c933ec232a81e9
YoungSeok Yoon <nullstein@gmail.com>
2014-06-28 23:59:01 -0400

    Fixes the problem where the journal entries cannot be saved when the Internet connection is bad (#31, #50)

--- Journaley/Models/Entry.cs
+++ Journaley/Models/Entry.cs
@@ -476,6 +476,7 @@
         public void Save(string folderPath)
         {
             XmlDocument doc = new XmlDocument();
+            doc.XmlResolver = null;
 
             // <?xml ...?>
             var decl = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
