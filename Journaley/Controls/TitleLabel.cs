217564febad0e1f4a5b1033dd59e234d2674d228
YoungSeok Yoon <nullstein@gmail.com>
2014-06-30 17:47:10 -0400

    Relay the mouse events to the parent for the title bar label. The title caption area can now be dragged.

--- Journaley/Controls/TitleLabel.cs
+++ Journaley/Controls/TitleLabel.cs
@@ -6,6 +6,7 @@
     using System.Linq;
     using System.Text;
     using System.Windows.Forms;
+    using Journaley.Utilities;
 
     /// <summary>
     /// TitleLabel class to force background fill.
@@ -24,6 +25,25 @@
             }
 
             base.OnPaint(e);
+        }
+
+        /// <summary>
+        /// Windows message loop
+        /// </summary>
+        /// <param name="m">The Windows <see cref="T:System.Windows.Forms.Message" /> to process.</param>
+        protected override void WndProc(ref Message m)
+        {
+            switch ((PInvoke.WindowsMessages)m.Msg)
+            {
+                // Relay the mouse events to its parent.
+                case PInvoke.WindowsMessages.WM_NCHITTEST:
+                    m.Result = (IntPtr)PInvoke.HitTestValues.HTTRANSPARENT;
+                    break;
+
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
         }
     }
 }
