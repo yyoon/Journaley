c9ce3277a3088bca23411d80d46d0cefff905f12
Sammy Guergachi <sguergachi@gmail.com>
2015-07-05 06:52:34 -0400

    Changed border style of password text field

--- Journaley/Forms/PasswordInputForm.Designer.cs
+++ Journaley/Forms/PasswordInputForm.Designer.cs
@@ -68,12 +68,12 @@
             // textBoxPassword
             // 
             this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(221)))), ((int)(((byte)(220)))));
-            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(129)));
             this.textBoxPassword.Location = new System.Drawing.Point(147, 109);
             this.textBoxPassword.Margin = new System.Windows.Forms.Padding(30);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(450, 48);
+            this.textBoxPassword.Size = new System.Drawing.Size(450, 55);
             this.textBoxPassword.TabIndex = 1;
             this.textBoxPassword.UseSystemPasswordChar = true;
             this.textBoxPassword.TextChanged += new System.EventHandler(this.TextBoxPassword_TextChanged);
