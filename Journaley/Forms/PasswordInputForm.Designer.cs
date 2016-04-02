612ba897886e34e903562809355ddd50286dd38d
Sammy Guergachi <sguergachi@gmail.com>
2016-04-02 01:09:55 -0400

    Added simulated padding to Password field.

--- Journaley/Forms/PasswordInputForm.Designer.cs
+++ Journaley/Forms/PasswordInputForm.Designer.cs
@@ -37,6 +37,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.imageButtonFormMinimize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageButtonFormMaximize)).BeginInit();
             this.panelContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormIcon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPressEnter)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,6 +45,7 @@
             // 
             this.panelTitlebar.Controls.Add(this.labelFormCaption);
             this.panelTitlebar.Size = new System.Drawing.Size(744, 20);
+            this.panelTitlebar.Controls.SetChildIndex(this.pictureBoxFormIcon, 0);
             this.panelTitlebar.Controls.SetChildIndex(this.imageButtonFormClose, 0);
             this.panelTitlebar.Controls.SetChildIndex(this.imageButtonFormMaximize, 0);
             this.panelTitlebar.Controls.SetChildIndex(this.imageButtonFormMinimize, 0);
@@ -75,12 +77,12 @@
             // textBoxPassword
             // 
             this.textBoxPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(221)))), ((int)(((byte)(220)))));
-            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPassword.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel, ((byte)(129)));
             this.textBoxPassword.Location = new System.Drawing.Point(147, 109);
             this.textBoxPassword.Margin = new System.Windows.Forms.Padding(30);
             this.textBoxPassword.Name = "textBoxPassword";
-            this.textBoxPassword.Size = new System.Drawing.Size(450, 55);
+            this.textBoxPassword.Size = new System.Drawing.Size(450, 48);
             this.textBoxPassword.TabIndex = 1;
             this.textBoxPassword.UseSystemPasswordChar = true;
             this.textBoxPassword.TextChanged += new System.EventHandler(this.TextBoxPassword_TextChanged);
@@ -123,6 +125,8 @@
             this.labelFormCaption.Text = "Journaley";
             this.labelFormCaption.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
+            // 
+            // 
             // PasswordInputForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -141,6 +145,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.imageButtonFormMaximize)).EndInit();
             this.panelContent.ResumeLayout(false);
             this.panelContent.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormIcon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxPressEnter)).EndInit();
             this.ResumeLayout(false);
 
